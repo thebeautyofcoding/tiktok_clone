32028cd883bd6dfe3576d872579fd3e50067ae73
Duong-Van-Tu <79387859+Duong-Van-Tu@users.noreply.github.com>
2023-10-10 17:20:10 +0700

    Update MainLayout.tsx

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -8,12 +8,7 @@ function MainLayout({ children }: { children: React.ReactNode }) {
       <header>
         <TopNav />
       </header>
-      <div
-        className={[
-          useLocation().pathname === "/" ? "max-w-[1140px]" : "",
-          "flex justify-between mx-auto w-full lg:px-2.5 px-0",
-        ].join(" ")}
-      >
+      <div>
         <div>
           <SideNavMain />
         </div>
